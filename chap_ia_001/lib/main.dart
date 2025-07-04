c14bba606659402406dfe668f72af4ceabd15c40
jholian10 <jholianmanuel10@gmail.com>
2025-07-04 22:16:58 +0200

    defino mi main con las clases que tengo ya creadas

--- chap_ia_001/lib/main.dart
+++ chap_ia_001/lib/main.dart
@@ -1,18 +1,23 @@
+import 'package:chap_ia_001/bloc/chap_bloc.dart';
 import 'package:chap_ia_001/views/chat_page.dart';
+import 'package:chap_ia_001/views/data/openai_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main() {
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
+  final OpenaiService api = OpenaiService();
+
+  MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ChatPage());
+    return MaterialApp(
+      title: 'Pregúntale a la IA',
+      home: BlocProvider(create: (_) => ChatBloc(api), child: ChatPage()),
+    );
   }
 }
