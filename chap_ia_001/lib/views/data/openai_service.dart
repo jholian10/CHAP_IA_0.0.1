e6dbab25c8718ec5a7079b3ad83f2a522fb083a4
jholian10 <jholianmanuel10@gmail.com>
2025-07-04 23:44:57 +0200

    ago. va  con la clave

--- chap_ia_001/lib/views/data/openai_service.dart
+++ chap_ia_001/lib/views/data/openai_service.dart
@@ -2,11 +2,12 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class OpenAIService {
-  final String _apiKey = "aqui va la clave de la ia "; // api de prova
+  final String _apiKey =
+      "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"; // api de prueva
 
   Future<String> sendMessage(String message) async {
     final response = await http.post(
-      Uri.parse("https://api.openai.com/v1/chat/completions"),
+      Uri.parse("https://api.openai.com/v1/chat/completions"), // peticion http
       headers: {
         "Content-Type": "application/json",
         "Authorization": "Bearer $_apiKey",
