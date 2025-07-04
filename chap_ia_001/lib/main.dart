65166e694463ff6f28f8fc3541f79ced3680d74c
jholian10 <jholianmanuel10@gmail.com>
2025-07-04 21:52:34 +0200

    empiezo el diseño se mis vistas

--- chap_ia_001/lib/main.dart
+++ chap_ia_001/lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:chap_ia_001/views/chat_page.dart';
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: ChatPage());
+  }
+}
---
+++ chap_ia_001/lib/views/loading_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoadingView extends StatefulWidget {
+  const LoadingView({super.key});
+
+  @override
+  State<LoadingView> createState() => _LoadingViewState();
+}
+
+class _LoadingViewState extends State<LoadingView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
